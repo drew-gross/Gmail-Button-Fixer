c0413e1bf9c12a2266176482fd43a8c136806c74
Drew Gross <drew.a.gross@gmail.com>
2014-05-21 19:47:24 -0400

    use XPATH

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 var buttonGrabber = setInterval(function() {
-	var buttons = document.getElementsByClassName('G-Ni');
-	if (buttons.length > 1) {
-		var refreshButton = buttons.item(3);
+	var refreshButton = document.evaluate("//*[@id=\":5\"]/div/div[1]/div[1]/div/div/div[4]",document,null,9,null).singleNodeValue;
+	if (refreshButton !== null) {
 		clearInterval(buttonGrabber);
 		var observer = new MutationObserver(function(nodes, observer) {
 			observer.disconnect();
