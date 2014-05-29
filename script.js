e6c44ed04107f8fdcff98e8fb267c7b60c80b95f
Drew Gross <drew.a.gross@gmail.com>
2014-05-29 12:23:19 -0400

    new xpath

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var buttonGrabber = setInterval(function() {
-	var refreshButton = document.evaluate("//*[@id=\":5\"]/div[3]/div[1]/div[1]/div/div/div[4]",document,null,9,null).singleNodeValue;
+	var refreshButton1 = document.evaluate("//*[@id=\":5\"]/div/div[1]/div[1]/div/div/div[4]",document,null,9,null).singleNodeValue;
+	var refreshButton2 = document.evaluate("//*[@id=\":5\"]/div[3]/div[1]/div[1]/div/div/div[4]",document,null,9,null).singleNodeValue;
+	var refreshButton = refreshButton1 || refreshButton2;
 	if (refreshButton !== null) {
 		clearInterval(buttonGrabber);
 		var observer = new MutationObserver(function(nodes, observer) {
